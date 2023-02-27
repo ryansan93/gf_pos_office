bdc7151a54fbc2ecf7cf28291573e08081148f7d
ryansan93 <ryansantoso93@gmail.com>
2023-02-27 12:40:49 +0700

    update laporan summary penjualan harian

--- application/modules/report/views/summary_penjualan_harian/export_pdf.php
+++ application/modules/report/views/summary_penjualan_harian/export_pdf.php
@@ -73,40 +73,42 @@
 						$tot14 = 0;
 					?>
 					<?php foreach ($data as $k_data => $v_data): ?>
-						<tr>
-							<td><?php echo isset($v_data['date']) ? tglIndonesia($v_data['date'], '-', ' ') : '-'; ?></td>
-							<td><?php echo isset($v_data['kode_faktur']) ? $v_data['kode_faktur'] : '-'; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_menu'][1]) ? angkaRibuan($v_data['kategori_menu'][1]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_menu'][2]) ? angkaRibuan($v_data['kategori_menu'][2]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_menu'][3]) ? angkaRibuan($v_data['kategori_menu'][3]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_menu'][4]) ? angkaRibuan($v_data['kategori_menu'][4]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_menu'][5]) ? angkaRibuan($v_data['kategori_menu'][5]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['diskon'][1]) ? angkaRibuan($v_data['diskon'][1]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['diskon'][2]) ? angkaRibuan($v_data['diskon'][2]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['total']) ? angkaRibuan($v_data['total']) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_pembayaran'][1]) ? angkaRibuan($v_data['kategori_pembayaran'][1]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_pembayaran'][2]) ? angkaRibuan($v_data['kategori_pembayaran'][2]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_pembayaran'][3]) ? angkaRibuan($v_data['kategori_pembayaran'][3]) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['kategori_pembayaran'][4]) ? angkaRibuan($v_data['kategori_pembayaran'][4]) : 0; ?></td>
-							<!-- <td align="right"><?php echo isset($v_data['diskon_requirement']['OC']) ? angkaRibuan($v_data['diskon_requirement']['OC']) : 0; ?></td>
-							<td align="right"><?php echo isset($v_data['diskon_requirement']['ENTERTAIN']) ? angkaRibuan($v_data['diskon_requirement']['ENTERTAIN']) : 0; ?></td> -->
-						</tr>
-						<?php
-							$tot1 += isset($v_data['kategori_menu'][1]) ? ($v_data['kategori_menu'][1]) : 0;
-							$tot2 += isset($v_data['kategori_menu'][2]) ? ($v_data['kategori_menu'][2]) : 0;
-							$tot3 += isset($v_data['kategori_menu'][3]) ? ($v_data['kategori_menu'][3]) : 0;
-							$tot5 += isset($v_data['kategori_menu'][5]) ? ($v_data['kategori_menu'][5]) : 0;
-							$tot4 += isset($v_data['diskon'][1]) ? ($v_data['diskon'][1]) : 0;
-							$tot6 += isset($v_data['diskon'][2]) ? ($v_data['diskon'][2]) : 0;
-							$tot7 += isset($v_data['total']) ? ($v_data['total']) : 0;
-							$tot8 += isset($v_data['kategori_pembayaran'][1]) ? ($v_data['kategori_pembayaran'][1]) : 0;
-							$tot9 += isset($v_data['kategori_pembayaran'][2]) ? ($v_data['kategori_pembayaran'][2]) : 0;
-							$tot10 += isset($v_data['kategori_pembayaran'][3]) ? ($v_data['kategori_pembayaran'][3]) : 0;
-							// $tot11 += isset($v_data['diskon_requirement']['OC']) ? ($v_data['diskon_requirement']['OC']) : 0;
-							// $tot12 += isset($v_data['diskon_requirement']['ENTERTAIN']) ? ($v_data['diskon_requirement']['ENTERTAIN']) : 0;
-							$tot13 += isset($v_data['kategori_pembayaran'][4]) ? ($v_data['kategori_pembayaran'][4]) : 0;
-							$tot14 += isset($v_data['kategori_menu'][4]) ? ($v_data['kategori_menu'][4]) : 0;
-						?>
+						<?php if ( (!isset($v_data['diskon_requirement']['OC']) || $v_data['diskon_requirement']['OC'] == 0) || (!isset($v_data['diskon_requirement']['ENTERTAIN']) && $v_data['diskon_requirement']['ENTERTAIN'] == 0) ) : ?>
+							<tr>
+								<td><?php echo isset($v_data['date']) ? tglIndonesia($v_data['date'], '-', ' ') : '-'; ?></td>
+								<td><?php echo isset($v_data['kode_faktur']) ? $v_data['kode_faktur'] : '-'; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_menu'][1]) ? angkaRibuan($v_data['kategori_menu'][1]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_menu'][2]) ? angkaRibuan($v_data['kategori_menu'][2]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_menu'][3]) ? angkaRibuan($v_data['kategori_menu'][3]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_menu'][4]) ? angkaRibuan($v_data['kategori_menu'][4]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_menu'][5]) ? angkaRibuan($v_data['kategori_menu'][5]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['diskon'][1]) ? angkaRibuan($v_data['diskon'][1]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['diskon'][2]) ? angkaRibuan($v_data['diskon'][2]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['total']) ? angkaRibuan($v_data['total']) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_pembayaran'][1]) ? angkaRibuan($v_data['kategori_pembayaran'][1]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_pembayaran'][2]) ? angkaRibuan($v_data['kategori_pembayaran'][2]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_pembayaran'][3]) ? angkaRibuan($v_data['kategori_pembayaran'][3]) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['kategori_pembayaran'][4]) ? angkaRibuan($v_data['kategori_pembayaran'][4]) : 0; ?></td>
+								<!-- <td align="right"><?php echo isset($v_data['diskon_requirement']['OC']) ? angkaRibuan($v_data['diskon_requirement']['OC']) : 0; ?></td>
+								<td align="right"><?php echo isset($v_data['diskon_requirement']['ENTERTAIN']) ? angkaRibuan($v_data['diskon_requirement']['ENTERTAIN']) : 0; ?></td> -->
+							</tr>
+							<?php
+								$tot1 += isset($v_data['kategori_menu'][1]) ? ($v_data['kategori_menu'][1]) : 0;
+								$tot2 += isset($v_data['kategori_menu'][2]) ? ($v_data['kategori_menu'][2]) : 0;
+								$tot3 += isset($v_data['kategori_menu'][3]) ? ($v_data['kategori_menu'][3]) : 0;
+								$tot5 += isset($v_data['kategori_menu'][5]) ? ($v_data['kategori_menu'][5]) : 0;
+								$tot4 += isset($v_data['diskon'][1]) ? ($v_data['diskon'][1]) : 0;
+								$tot6 += isset($v_data['diskon'][2]) ? ($v_data['diskon'][2]) : 0;
+								$tot7 += isset($v_data['total']) ? ($v_data['total']) : 0;
+								$tot8 += isset($v_data['kategori_pembayaran'][1]) ? ($v_data['kategori_pembayaran'][1]) : 0;
+								$tot9 += isset($v_data['kategori_pembayaran'][2]) ? ($v_data['kategori_pembayaran'][2]) : 0;
+								$tot10 += isset($v_data['kategori_pembayaran'][3]) ? ($v_data['kategori_pembayaran'][3]) : 0;
+								// $tot11 += isset($v_data['diskon_requirement']['OC']) ? ($v_data['diskon_requirement']['OC']) : 0;
+								// $tot12 += isset($v_data['diskon_requirement']['ENTERTAIN']) ? ($v_data['diskon_requirement']['ENTERTAIN']) : 0;
+								$tot13 += isset($v_data['kategori_pembayaran'][4]) ? ($v_data['kategori_pembayaran'][4]) : 0;
+								$tot14 += isset($v_data['kategori_menu'][4]) ? ($v_data['kategori_menu'][4]) : 0;
+							?>
+						<?php endif ?>
 					<?php endforeach ?>
 					<tr>
 						<td align="right" colspan="2"><b>Total</b></td>
