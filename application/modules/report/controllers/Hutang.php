4bc01433a9d55c6b91d3df4928dbcbbc67e95e2b
ryansan93 <ryansantoso93@gmail.com>
2023-05-11 09:31:02 +0700

    update laporan hutang

--- application/modules/report/controllers/Hutang.php
+++ application/modules/report/controllers/Hutang.php
@@ -150,7 +150,7 @@ class Hutang extends Public_Controller {
                         jual.kode_faktur_utama = j.kode_faktur
                 where
                     jual.kode_faktur is not null and
-                    j.hutang = 1
+                    (j.hutang = 1 or lunas = 0)
                 group by 
                     jual.kode_faktur_utama,
                     j.lunas,
