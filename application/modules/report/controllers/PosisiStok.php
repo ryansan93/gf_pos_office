2d13bb018f4c2f826a1d38fcfff994f2e9c72263
ryansan93 <ryansantoso93@gmail.com>
2023-07-07 17:35:06 +0700

    update laporan posisi stok

--- application/modules/report/controllers/PosisiStok.php
+++ application/modules/report/controllers/PosisiStok.php
@@ -244,6 +244,77 @@ class PosisiStok extends Public_Controller {
                         ksort( $data[ $v_data['gudang_kode'] ]['group_item'][ $v_det['group_kode'] ]['detail'] );
                         ksort( $data[ $v_data['gudang_kode'] ]['group_item'][ $v_det['group_kode'] ]['detail'][ $key_item ]['detail_tanggal'] );
                     }
+                } else {
+                    foreach ($_item as $k_item => $v_item) {
+                        $conf = new \Model\Storage\Conf();
+                        $sql = "
+                            select top 1
+                                i.kode,
+                                i.nama,
+                                i.group_kode,
+                                gi.nama as group_nama,
+                                st.satuan
+                            from item i
+                            right join
+                                group_item gi
+                                on
+                                    i.group_kode = gi.kode
+                            left join
+                                item_satuan st
+                                on
+                                    st.item_kode = i.kode
+                            where
+                                i.kode = '".$v_item."' and
+                                st.pengali = 1
+                        ";
+                        $d_item = $conf->hydrateRaw($sql);
+
+                        $nama_item = null;
+                        $satuan = null;
+                        $group_kode = null;
+                        $group_nama = null;
+                        if ( $d_item->count() > 0 ) {
+                            $nama_item = $d_item->toArray()[0]['nama'];
+                            $satuan = $d_item->toArray()[0]['satuan'];
+                            $group_kode = $d_item->toArray()[0]['group_kode'];
+                            $group_nama = $d_item->toArray()[0]['group_nama'];
+                        }
+
+                        $conf = new \Model\Storage\Conf();
+                        $sql = "
+                            select top 1
+                                * 
+                            from stok_harga sh
+                            where
+                                sh.id_header = ".$id_stok_tanggal." and
+                                sh.item_kode = '".$v_item."'
+                        ";
+                        $d_harga = $conf->hydrateRaw($sql);
+
+                        $harga = 0;
+                        if ( $d_harga->count() > 0 ) {
+                            $harga = $d_harga->toArray()[0]['harga'];
+                        }
+
+                        $key_item = $nama_item.' | '.$v_item;
+
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['kode'] = $group_kode;
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['nama'] = $group_nama;
+
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['kode'] = $v_item;
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['nama'] = $nama_item;
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['satuan'] = $satuan;
+
+                        $key_tanggal = str_replace('-', '', substr($v_data['tanggal'], 0, 10));
+
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['detail_tanggal'][ $key_tanggal ]['tanggal'] = $v_data['tanggal'];
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['detail_tanggal'][ $key_tanggal ]['jumlah'] = 0;
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['detail_tanggal'][ $key_tanggal ]['harga'] = $harga;
+                        $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['detail_tanggal'][ $key_tanggal ]['nilai_stok'] = 0;
+
+                        ksort( $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'] );
+                        ksort( $data[ $v_data['gudang_kode'] ]['group_item'][ $group_kode ]['detail'][ $key_item ]['detail_tanggal'] );
+                    }
                 }
             }
         }
