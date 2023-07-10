8fbad7421bc69302da9f3313ffb1ddfed4e8a755
ryansan93 <ryansantoso93@gmail.com>
2023-07-10 14:14:16 +0700

    update fitur stok opname

--- application/modules/transaksi/controllers/StokOpname.php
+++ application/modules/transaksi/controllers/StokOpname.php
@@ -883,7 +883,7 @@ class StokOpname extends Public_Controller {
 
         $sql = "EXEC sp_hitung_stok_by_barang @barang = '".str_replace('"', '', str_replace(']', '', str_replace('[', '', json_encode($barang))))."', @tgl_transaksi = '".$tgl_transaksi."', @gudang = '".str_replace('"', '', str_replace(']', '', str_replace('[', '', json_encode($gudang))))."'";
 
-        // cetak_r( $sql, 1 );
+        cetak_r( $sql, 1 );
 
         $d_conf = $m_conf->hydrateRaw($sql);
     }
