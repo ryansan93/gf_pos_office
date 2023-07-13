09cdb43b241cf17aa992200da49da3400fb8eeab
ryansan93 <ryansantoso93@gmail.com>
2023-07-13 12:58:53 +0700

    update laporan penerimaan barang

--- application/modules/report/views/penerimaan/export_excel.php
+++ application/modules/report/views/penerimaan/export_excel.php
@@ -42,34 +42,36 @@
 					<?php $total_per_kode = 0; ?>
 					<?php foreach ($v_kode['detail'] as $k_det => $v_det): ?>
 						<tr>
-							<td class="text-center"><?php echo tglIndonesia($v_det['tgl_terima'], '-', ' '); ?></td>
-							<td class="text-center"><?php echo $v_det['kode_terima']; ?></td>
-							<td class="text-center"><?php echo $v_det['po_no']; ?></td>
+							<td align="center"><?php echo tglIndonesia($v_det['tgl_terima'], '-', ' '); ?></td>
+							<td align="center"><?php echo $v_det['kode_terima']; ?></td>
+							<td align="center"><?php echo $v_det['po_no']; ?></td>
 							<td><?php echo $v_det['supplier']; ?></td>
 							<td><?php echo $v_det['nama_gudang']; ?></td>
 							<td><?php echo $v_det['nama_item']; ?></td>
 							<td><?php echo $v_det['coa']; ?></td>
 							<td><?php echo $v_det['satuan']; ?></td>
-							<td class="text-right"><?php echo angkaDecimal($v_det['jumlah_terima']); ?></td>
-							<td class="text-right"><?php echo angkaDecimal($v_det['harga']); ?></td>
+							<td align="right"><?php echo angkaDecimal($v_det['jumlah_terima']); ?></td>
+							<td align="right"><?php echo angkaDecimal($v_det['harga']); ?></td>
 							<?php $total = $v_det['jumlah_terima'] * $v_det['harga']; ?>
 							<?php $grand_total += $total; ?>
-							<td class="text-right"><?php echo angkaDecimal($total); ?></td>
+							<?php $total_per_tanggal += $total; ?>
+							<?php $total_per_kode += $total; ?>
+							<td align="right"><?php echo angkaDecimal($total); ?></td>
 						</tr>
 					<?php endforeach ?>
 						<tr>
-						<td class="text-right" colspan="10"><b>TOTAL PER KODE - <?php echo $v_det['kode_terima']; ?></b></td>
-						<td class="text-right"><b><?php echo angkaDecimal($total_per_kode); ?></b></td>
+						<td align="right" colspan="10"><b>TOTAL PER KODE - <?php echo $v_det['kode_terima']; ?></b></td>
+						<td align="right"><b><?php echo angkaDecimal($total_per_kode); ?></b></td>
 					</tr>
 				<?php endforeach ?>
 				<tr>
-					<td class="text-right" colspan="10"><b>TOTAL PER TANGGAL - <?php echo tglIndonesia($v_det['tgl_terima'], '-', ' '); ?></b></td>
-					<td class="text-right"><b><?php echo angkaDecimal($total_per_tanggal); ?></b></td>
+					<td align="right" colspan="10"><b>TOTAL PER TANGGAL - <?php echo tglIndonesia($v_det['tgl_terima'], '-', ' '); ?></b></td>
+					<td align="right"><b><?php echo angkaDecimal($total_per_tanggal); ?></b></td>
 				</tr>
 			<?php endforeach ?>
 			<tr>
-				<td class="text-right" colspan="10" align="right"><b>TOTAL</b></td>
-				<td class="text-right"><b><?php echo angkaDecimal($grand_total); ?></b></td>
+				<td align="right" colspan="10"><b>TOTAL</b></td>
+				<td align="right"><b><?php echo angkaDecimal($grand_total); ?></b></td>
 			</tr>
 		<?php else: ?>
 			<tr>
