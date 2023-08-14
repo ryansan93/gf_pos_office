3e562174c2c90b324eefadb9b9fd78cdf9f47756
ryansan93 <ryansantoso93@gmail.com>
2023-08-14 15:24:07 +0700

    update query summary penjualan harian

--- application/modules/report/controllers/SummaryPenjualanHarian.php
+++ application/modules/report/controllers/SummaryPenjualanHarian.php
@@ -670,13 +670,9 @@ class SummaryPenjualanHarian extends Public_Controller {
                     $data[ $key ]['kategori_pembayaran'][3] += ($value['id'] == 3) ? $value['nilai'] : 0;
                     $data[ $key ]['kategori_pembayaran'][4] += ($value['id'] == 4) ? $value['nilai'] : 0;
 
-                    if ( 
-                        $data[ $key ]['kategori_pembayaran'][1] > 0 ||
-                        $data[ $key ]['kategori_pembayaran'][2] > 0 ||
-                        $data[ $key ]['kategori_pembayaran'][3] > 0
-                    ) {
-                        $data[ $key ]['kategori_pembayaran'][4] = 0;
-                    }
+                    // if ($data[ $key ]['kategori_pembayaran'][1] > 0 || $data[ $key ]['kategori_pembayaran'][2] > 0 || $data[ $key ]['kategori_pembayaran'][3] > 0) {
+                    //     $data[ $key ]['kategori_pembayaran'][4] = 0;
+                    // }
 
                     // if ( $value['id'] != 4 ) {
                     //     if ( isset($data[ $key ]['kategori_pembayaran'][4]) && $data[ $key ]['kategori_pembayaran'][4] > 0 ) {
@@ -702,6 +698,9 @@ class SummaryPenjualanHarian extends Public_Controller {
                 //     $data[ $key ]['kategori_pembayaran'][3] = 0;
                 // }
             }
+
+            $total = $data[ $key ]['kategori_pembayaran'][1] + $data[ $key ]['kategori_pembayaran'][2] + $data[ $key ]['kategori_pembayaran'][3];
+            $data[ $key ]['kategori_pembayaran'][4] -= $total;
         }
 
         // $sql = "
