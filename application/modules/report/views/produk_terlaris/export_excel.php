05461b80ca2a352078706fcea52b84712e5b0449
ryansan93 <ryansantoso93@gmail.com>
2023-09-30 12:48:38 +0700

    update export excel produk terlaris

--- application/modules/report/views/produk_terlaris/export_excel.php
+++ application/modules/report/views/produk_terlaris/export_excel.php
@@ -24,12 +24,12 @@
                 <?php $no = 1; ?>
                 <?php foreach ($data as $k_data => $v_data): ?>
                     <tr class="search">
-                        <td class="number_format"><?php echo angkaRibuan($no); ?></td>
+                        <td class="number_format"><?php echo ($no); ?></td>
                         <td class="str"><?php echo !empty($v_data['kategori']) ? $v_data['kategori'] : '-'; ?></td>
                         <td class="str"><?php echo $v_data['jenis']; ?></td>
                         <td class="str"><?php echo $v_data['menu_nama']; ?></td>
-                        <td class="number_format"><?php echo angkaRibuan($v_data['qty']); ?></td>
-                        <td class="decimal_number_format"><?php echo angkaDecimal($v_data['total']); ?></td>
+                        <td class="number_format"><?php echo ($v_data['qty']); ?></td>
+                        <td class="decimal_number_format"><?php echo ($v_data['total']); ?></td>
                     </tr>
                     <?php $no++; ?>
                 <?php endforeach ?>
@@ -62,15 +62,15 @@
                     <?php foreach ($v_member['detail_menu'] as $k_data => $v_data): ?>
                         <tr class="search">
                             <?php if ( $idx_member == 0 ): ?>
-                                <td class="number_format" rowspan="<?php echo count($v_member['detail_menu'])+1; ?>"><?php echo angkaRibuan($no); ?></td>
+                                <td class="number_format" rowspan="<?php echo count($v_member['detail_menu'])+1; ?>"><?php echo ($no); ?></td>
                                 <td class="str" rowspan="<?php echo count($v_member['detail_menu']); ?>"><?php echo $v_member['nama']; ?></td>
                                 <td class="str" rowspan="<?php echo count($v_member['detail_menu']); ?>" class="text-right"><?php echo $v_member['jml_transaksi']; ?></td>
                             <?php endif ?>
                             <td class="str"><?php echo !empty($v_data['kategori']) ? $v_data['kategori'] : '-'; ?></td>
                             <td class="str"><?php echo $v_data['jenis']; ?></td>
                             <td class="str"><?php echo $v_data['menu_nama']; ?></td>
-                            <td class="number_format"><?php echo angkaRibuan($v_data['qty']); ?></td>
-                            <td class="decimal_number_format"><?php echo angkaDecimal($v_data['total']); ?></td>
+                            <td class="number_format"><?php echo ($v_data['qty']); ?></td>
+                            <td class="decimal_number_format"><?php echo ($v_data['total']); ?></td>
                         </tr>
                         <?php
                             $t_jumlah += $v_data['qty']; 
@@ -80,8 +80,8 @@
                     <?php endforeach ?>
                     <tr>
                         <td colspan="5" class="str"><b>TOTAL</b></td>
-                        <td class="number_format"><b><?php echo angkaRibuan($t_jumlah); ?></b></td>
-                        <td class="decimal_number_format"><b><?php echo angkaDecimal($t_total); ?></b></td>
+                        <td class="number_format"><b><?php echo ($t_jumlah); ?></b></td>
+                        <td class="decimal_number_format"><b><?php echo ($t_total); ?></b></td>
                     </tr>
                     <?php $no++; ?>
                 <?php endforeach ?>
