697bcd2b8a9e2d94c7c6b194274c4cf5748971f9
ryansan93 <ryansantoso93@gmail.com>
2023-10-04 16:31:24 +0700

    update export excel mutasi barang

--- application/modules/transaksi/views/penerimaan_barang_mutasi/export_excel.php
+++ application/modules/transaksi/views/penerimaan_barang_mutasi/export_excel.php
@@ -2,8 +2,8 @@
     <head>
         <style type="text/css">
             .str { mso-number-format:\@; }
-            .decimal_number_format { mso-number-format: "\#\,\#\#0\.##"; }
-            /* .decimal_number_format { mso-number-format: "\#\,\#\##.##"; } */
+            /* .decimal_number_format { mso-number-format: "\#\,\#\#0\.##"; } */
+            .decimal_number_format { mso-number-format: "\#\,\#\#0.##"; }
             /* .decimal_number_format { mso-number-format: "#,###.##"; } */
             .number_format { mso-number-format: "\#\,\#\#0"; }
         </style>
