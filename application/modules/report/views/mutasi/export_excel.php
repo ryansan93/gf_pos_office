10b5c1cbdc1a29af4eacd55b7601d950465e11ca
ryansan93 <ryansantoso93@gmail.com>
2023-10-04 14:42:08 +0700

    update export excel mutasi barang

--- application/modules/report/views/mutasi/export_excel.php
+++ application/modules/report/views/mutasi/export_excel.php
@@ -59,7 +59,8 @@
 							<td><?php echo $v_det['coa']; ?></td>
 							<td><?php echo $v_det['satuan']; ?></td>
 							<td class="decimal_number_format" align="right"><?php echo ($v_det['jumlah']); ?></td>
-							<td class="decimal_number_format4" align="right"><?php echo ($v_det['harga']); ?></td>
+							<!-- <td class="decimal_number_format4" align="right"><?php echo ($v_det['harga']); ?></td> -->
+							<td class="decimal_number_format" align="right"><?php echo number_format((float)$v_det['harga'], 2, '.', ''); ?></td>
 							<td class="decimal_number_format" align="right"><?php echo ($total); ?></td>
 						</tr>
 					<?php endforeach ?>
