32ca3ad8589cde23f75ca7da66ac7a3b954daa06
ryansan93 <ryansantoso93@gmail.com>
2023-10-04 14:29:14 +0700

    update export excel mutasi barang

--- application/modules/report/views/mutasi/export_excel.php
+++ application/modules/report/views/mutasi/export_excel.php
@@ -25,16 +25,16 @@
 <table border="1">
 	<thead>
 		<tr>
-			<th class="col-xs-1">Tanggal</th>
-			<th class="col-xs-1">Kode Mutasi</th>
-			<th class="col-xs-1">Asal</th>
-			<th class="col-xs-1">Tujuan</th>
-			<th class="col-xs-2">Nama Item</th>
-			<th class="col-xs-1">COA SAP</th>
-			<th class="col-xs-1">Satuan</th>
-			<th class="col-xs-1">Jumlah</th>
-			<th class="col-xs-1">Harga (Rp.)</th>
-			<th class="col-xs-1">Nilai</th>
+			<th>Tanggal</th>
+			<th>Kode Mutasi</th>
+			<th>Asal</th>
+			<th>Tujuan</th>
+			<th>Nama Item</th>
+			<th>COA SAP</th>
+			<th>Satuan</th>
+			<th>Jumlah</th>
+			<th>Harga (Rp.)</th>
+			<th>Nilai</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -45,9 +45,13 @@
 				<?php foreach ($v_tanggal['detail'] as $k_kode => $v_kode): ?>
 					<?php $total_per_kode = 0; ?>
 					<?php foreach ($v_kode['detail'] as $k_det => $v_det): ?>
+						<?php $total = $v_det['jumlah'] * $v_det['harga']; ?>
+						<?php $grand_total += $total; ?>
+						<?php $total_per_tanggal += $total; ?>
+						<?php $total_per_kode += $total; ?>
 						<tr>
-							<td class="text-center"><?php echo $v_det['tgl_mutasi']; ?></td>
-							<td class="text-center"><?php echo $v_det['kode_mutasi']; ?></td>
+							<td><?php echo $v_det['tgl_mutasi']; ?></td>
+							<td><?php echo $v_det['kode_mutasi']; ?></td>
 							<td><?php echo $v_det['nama_gudang_asal']; ?></td>
 							<td><?php echo $v_det['nama_gudang_tujuan']; ?></td>
 							<td><?php echo $v_det['nama_item']; ?></td>
@@ -55,10 +59,6 @@
 							<td><?php echo $v_det['satuan']; ?></td>
 							<td class="decimal_number_format" align="right"><?php echo ($v_det['jumlah']); ?></td>
 							<td class="decimal_number_format" align="right"><?php echo ($v_det['harga']); ?></td>
-							<?php $total = $v_det['jumlah'] * $v_det['harga']; ?>
-							<?php $grand_total += $total; ?>
-							<?php $total_per_tanggal += $total; ?>
-							<?php $total_per_kode += $total; ?>
 							<td class="decimal_number_format" align="right"><?php echo ($total); ?></td>
 						</tr>
 					<?php endforeach ?>
