f6253e98106b37b05c13d6d7956cde957bc92b6b
ryansan93 <ryansantoso93@gmail.com>
2023-10-04 15:41:16 +0700

    update export excel

--- application/modules/transaksi/views/penerimaan_barang_mutasi/export_excel.php
+++ application/modules/transaksi/views/penerimaan_barang_mutasi/export_excel.php
@@ -1,8 +1,6 @@
 <style type="text/css">
 	.str { mso-number-format:\@; }
-	.decimal_number_format { mso-number-format:"0.00"; }
-	/* .decimal_number_format { mso-number-format: "\#\,\#\#0.00"; }
-	.decimal_number_format4 { mso-number-format: "\#\,\#\#0.0000"; } */
+	.decimal_number_format { mso-number-format: "\#\,\#\#0.00"; }
 	.number_format { mso-number-format: "\#\,\#\#0"; }
 </style>
 <table border="1">
@@ -65,7 +63,7 @@
                         ?>
                     </td>
                     <td class="str"><?php // echo ($v_data['g_status'] == getStatus('submit')) ? 'BELUM' : 'SUDAH'; ?></td>
-                    <td class="decimal_number_format"><?php echo (float)$v_data['total']; ?></td>
+                    <td class="decimal_number_format"><?php echo ((float)$v_data['total']); ?></td>
                 </tr>
             <?php endforeach ?>
         <?php else: ?>
