d3410fef325227488d5ac44dc192acaadc45a61d
ryansan93 <ryansantoso93@gmail.com>
2023-10-04 15:30:18 +0700

    update export excel

--- application/modules/transaksi/views/penerimaan_barang_mutasi/export_excel.php
+++ application/modules/transaksi/views/penerimaan_barang_mutasi/export_excel.php
@@ -1,7 +1,6 @@
 <style type="text/css">
 	.str { mso-number-format:\@; }
 	.decimal_number_format { mso-number-format: "\#\,\#\#0.00"; }
-	.decimal_number_format4 { mso-number-format: "\#\,\#\#0.0000"; }
 	.number_format { mso-number-format: "\#\,\#\#0"; }
 </style>
 <table border="1">
@@ -29,42 +28,42 @@
                     <td class="str"><?php echo strtoupper($v_data['nama_gudang_tujuan']); ?></td>
                     <td class="str">
                         <?php
-                            if ( !empty($v_data['list_coa']) ) {
-                                $idx = 0;
-                                foreach ($v_data['list_coa'] as $key => $value) {
-                                    if ( $idx == 0 ) {
-                                        echo $value['coa'];
-                                    } else {
-                                        echo '<br>'.$value['coa'];
-                                    }
+                            // if ( !empty($v_data['list_coa']) ) {
+                            //     $idx = 0;
+                            //     foreach ($v_data['list_coa'] as $key => $value) {
+                            //         if ( $idx == 0 ) {
+                            //             echo $value['coa'];
+                            //         } else {
+                            //             echo '<br>'.$value['coa'];
+                            //         }
 
-                                    $idx++;
-                                }
-                            } else {
-                                echo '-';
-                            }
+                            //         $idx++;
+                            //     }
+                            // } else {
+                            //     echo '-';
+                            // }
                         ?>
                     </td>
                     <td class="str">
                         <?php
-                            if ( !empty($v_data['list_coa']) ) {
-                                $idx = 0;
-                                foreach ($v_data['list_coa'] as $key => $value) {
-                                    if ( $idx == 0 ) {
-                                        echo $value['ket_coa'];
-                                    } else {
-                                        echo '<br>'.$value['ket_coa'];
-                                    }
+                            // if ( !empty($v_data['list_coa']) ) {
+                            //     $idx = 0;
+                            //     foreach ($v_data['list_coa'] as $key => $value) {
+                            //         if ( $idx == 0 ) {
+                            //             echo $value['ket_coa'];
+                            //         } else {
+                            //             echo '<br>'.$value['ket_coa'];
+                            //         }
 
-                                    $idx++;
-                                }
-                            } else {
-                                echo '-';
-                            }
+                            //         $idx++;
+                            //     }
+                            // } else {
+                            //     echo '-';
+                            // }
                         ?>
                     </td>
-                    <td class="str"><?php echo ($v_data['g_status'] == getStatus('submit')) ? 'BELUM' : 'SUDAH'; ?></td>
-                    <td class="decimal_number_format"><?php echo $v_data['total']; ?></td>
+                    <td class="str"><?php // echo ($v_data['g_status'] == getStatus('submit')) ? 'BELUM' : 'SUDAH'; ?></td>
+                    <td class=""><?php echo $v_data['total']; ?></td>
                 </tr>
             <?php endforeach ?>
         <?php else: ?>
