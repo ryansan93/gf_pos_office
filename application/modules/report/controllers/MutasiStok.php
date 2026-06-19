06d933b436aefd4b9882528db213aae7b9d050a5
laptop kantor <ryansantoso93@gmail.com>
2026-06-20 02:12:21 +0700

    update mutasi stok

--- application/modules/report/controllers/MutasiStok.php
+++ application/modules/report/controllers/MutasiStok.php
@@ -177,8 +177,7 @@ class MutasiStok extends Public_Controller {
                                 on
                                     st.id = s.id_header
                             where
-                                -- st.tanggal between '".$start_date."' and '".$end_date."' and
-                                st.tanggal <= '".$start_date."' and
+                                st.tanggal between '".$start_date."' and '".$end_date."' and
                                 s.tanggal < '".$start_date."' and
                                 st.gudang_kode = '".$_gudang."'
                                 ".$sql_item."
@@ -215,8 +214,7 @@ class MutasiStok extends Public_Controller {
                             on
                                 s.id = strans.id_header
                         where
-                            -- st.tanggal between '".$start_date."' and '".$end_date."' and
-                            st.tanggal <= '".$start_date."' and
+                            st.tanggal between '".$start_date."' and '".$end_date."' and
                             st.gudang_kode = '".$_gudang."'
                             ".$sql_item."
                         group by
